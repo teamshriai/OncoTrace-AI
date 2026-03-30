a22bf3f87a4385bb2dcb0c750cfc3dd1f34127c5
teamshriai <teamshriai@gmail.com>
2026-03-30 09:37:12 +0530

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import DNA3DSection from "./components/DNA3DSection";
@@ -65,3 +66,77 @@ export default function App() {
     </div>
   );
 }
+=======
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import DNA3DSection from "./components/DNA3DSection";
+import Mammogram from "./components/Mammogram";
+import ProblemSection from "./components/ProblemSection";
+import SolutionSection from "./components/SolutionSection";
+import CaseStudySection from "./components/CaseStudySection";
+import PartnersSection from "./components/PartnersSection";
+import TeamSection from "./components/TeamSection";
+import CTASection from "./components/CTASection";
+import Footer from "./components/Footer";
+import LiquidBiopsySection from "./components/LiquidBiopsySection";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white antialiased scroll-smooth">
+      <Navbar />
+      <main>
+        <HeroSection />
+
+        <div className="h-32 bg-white" />
+
+        <DNA3DSection />
+
+        <div className="relative">
+          <span id="mammogram" className="absolute -top-24 block" aria-hidden="true" />
+          <Mammogram />
+        </div>
+
+        <div className="relative">
+          <span id="LiquidBiopsySection" className="absolute -top-24 block" aria-hidden="true" />
+          <LiquidBiopsySection />
+        </div>
+
+        <ProblemSection />
+
+        <div className="relative">
+          <span id="solution" className="absolute -top-24 block" aria-hidden="true" />
+          <SolutionSection />
+        </div>
+
+        <div className="relative">
+
+
+          <span id="liquid-biopsy" className="absolute -top-24 block" aria-hidden="true" />
+          <LiquidBiopsySection />
+        </div>
+
+        <div className="relative">
+          <span id="case-study" className="absolute -top-24 block" aria-hidden="true" />
+          <CaseStudySection />
+        </div>
+
+        <div className="relative">
+          <span id="partners" className="absolute -top-24 block" aria-hidden="true" />
+          <PartnersSection />
+        </div>
+
+        <div className="relative">
+          <span id="team" className="absolute -top-24 block" aria-hidden="true" />
+          <TeamSection />
+        </div>
+
+        <div className="relative">
+          <span id="cta" className="absolute -top-24 block" aria-hidden="true" />
+          <CTASection />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+>>>>>>> e4cbe3b (Initial commit)
